77962bc30adb0e981ae51ada1f3cf7f18c3697ad
Ben MartinMooney <ben.martinmooney@gmail.com>
2021-01-26 16:57:05 -0500

    index-page styling
    Oof.

--- client/src/components/CategoryTile.js
+++ client/src/components/CategoryTile.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const CategoryTile = ({category}) => {
   return (
-    <div>
+    <div className="category-tile cell small-6">
       <h4>
         {category}
       </h4>
