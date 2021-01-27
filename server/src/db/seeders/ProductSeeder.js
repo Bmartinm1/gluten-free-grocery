b3b85d397da76d9ecf9676332be165e4ac3e89ec
MC4569 <mcollins891@gmail.com>
2021-01-27 17:09:47 -0500

    updated ProductSeeder

--- server/src/db/seeders/ProductSeeder.js
+++ server/src/db/seeders/ProductSeeder.js
@@ -1,36 +1,41 @@
-import Product from "../../models/Product.js"
+import { Product, Category } from "../../models/index.js"
 
 class ProductSeeder {
   static async seed() {
+    const bread = await Category.query().findOne({ name: "Bread & Bakery" })
+    const cereal = await Category.query().findOne({ name: "Breakfast & Cereal" })
+    const soup = await Category.query().findOne({ name: "Canned Goods & Soups" })
+    const baking = await Category.query().findOne({ name: "Condiments/Spices & Baking" })
+
     const productsData = [
       {
         productName: "Heritage Style Whole Grain Bread Gluten Free",
         brandName: "Canyon Bakehouse",
         description: "Certified Gluten Free. Dairy, Soy, and nut free.",
-        categoryId: "1"
+        categoryId: bread.id
       },
       {
         productName: "Artisan Baker White Bread Gluten Free",
         brandName: "Schar",
         description: "Baked with Ancient Grains.",
-        categoryId: "1"
+        categoryId: bread.id
       },
       {
         productName: "Corn Cereal",
         brandName: "Chex",
         description: "Oven toasted Corn Cereal",
-        categoryId: "2"
+        categoryId: cereal.id
       },
       {
         productName: "New England Clam Chowder Soup",
         brandName: "Progresso Rich & Hearty",
-        categoryId: "3"
+        categoryId: soup.id
       },
       {
         productName: "Gluten Free Measure For Measure Flour",
         brandName: "King Arthur",
         description: "Substitute 1:1 for Standard all-purpose White Flour",
-        categoryId: "4"
+        categoryId: baking.id
       }
     ]
 
