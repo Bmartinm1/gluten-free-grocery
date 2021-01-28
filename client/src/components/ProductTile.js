407ae0e23668ad67d82c9e4267991a62f9f588fb
maeghanpro <maeghan.m.provencher@gmail.com>
2021-01-28 16:54:54 -0500

    added link to product show

--- client/src/components/ProductTile.js
+++ client/src/components/ProductTile.js
@@ -1,10 +1,13 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const ProductTile = ({product}) => {
   return (
     <div>
+      <Link to={`/products/${product.id}`}>
       <h4>{product.brandName}, {product.productName}</h4>
       <p>{product.description}</p>
+      </Link>
     </div>
   )
 }
