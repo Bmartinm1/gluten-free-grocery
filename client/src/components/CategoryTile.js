6ba922320b2e43d7736385df4f66d30aebfca1ae
maeghanpro <62575735+maeghanpro@users.noreply.github.com>
2021-01-28 17:00:06 -0500

    Update CategoryTile.js

--- client/src/components/CategoryTile.js
+++ client/src/components/CategoryTile.js
@@ -5,7 +5,6 @@ const CategoryTile = ({category}) => {
   return (
     <div className="category-tile cell small-6">
       <Link to={`/categories/${category.id}`}>
-
         <h4>
           {category.name}
         </h4>
