2ee48931d80d2d060df99dc49e1437ada3a94ab3
maeghanpro <62575735+maeghanpro@users.noreply.github.com>
2021-01-28 17:02:14 -0500

    Update categoriesRouter.js

--- server/src/routes/api/v1/categoriesRouter.js
+++ server/src/routes/api/v1/categoriesRouter.js
@@ -7,14 +7,11 @@ const categoriesRouter = new express.Router()
 
 categoriesRouter.get('/', async (req, res) => {
   try {
-    
     const categories = await Category.query()
     const serializedCategories = categories.map(category => {
       return CategorySerializer.getSummary(category)
     })
-
     return res.status(200).json({categories: serializedCategories })
-
   } catch (error) {
     return res.status(500).json({errors: error})
   }
