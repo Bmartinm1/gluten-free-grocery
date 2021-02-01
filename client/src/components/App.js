f98fe49efa34efd6eeadd1eb048aa16085c8187f
Ben MartinMooney <ben.martinmooney@gmail.com>
2021-02-01 14:06:03 -0500

    double to single quotes for continuity

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,15 +1,15 @@
-import React, { useState, useEffect } from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import { hot } from "react-hot-loader/root";
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { hot } from 'react-hot-loader/root';
 
-import getCurrentUser from "../services/getCurrentUser";
-import "../assets/scss/main.scss";
-import RegistrationForm from "./registration/RegistrationForm";
-import SignInForm from "./authentication/SignInForm";
-import TopBar from "./layout/TopBar";
+import getCurrentUser from '../services/getCurrentUser';
+import '../assets/scss/main.scss';
+import RegistrationForm from './registration/RegistrationForm';
+import SignInForm from './authentication/SignInForm';
+import TopBar from './layout/TopBar';
 import CategoryIndex from './CategoryIndex';
 import ProductShow from './ProductShow'
-import CategoryShowPage from "./CategoryShowPage"
+import CategoryShowPage from './CategoryShowPage'
 import NewProductForm from './NewProductForm'
 
 const App = (props) => {
@@ -32,12 +32,12 @@ const App = (props) => {
     <Router>
       <TopBar user={ currentUser } />
       <Switch>
-        <Route exact path="/" component={ CategoryIndex } />
-        <Route exact path="/users/new" component={ RegistrationForm } />
-        <Route exact path="/user-sessions/new" component={ SignInForm } />
-        <Route exact path="/products/:id" component={ ProductShow } />
-        <Route exact path="/categories/:id" component={CategoryShowPage} />
-        <Route exact path="/categories/:categoryId/products/new" component={NewProductForm} />
+        <Route exact path='/' component={ CategoryIndex } />
+        <Route exact path='/users/new' component={ RegistrationForm } />
+        <Route exact path='/user-sessions/new' component={ SignInForm } />
+        <Route exact path='/products/:id' component={ ProductShow } />
+        <Route exact path='/categories/:id' component={CategoryShowPage} />
+        <Route exact path='/categories/:categoryId/products/new' component={NewProductForm} />
       </Switch>
     </Router>
   );
