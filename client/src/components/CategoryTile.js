01acc02c51d607237c47105c3720accc0258cfaf
Ben MartinMooney <ben.martinmooney@gmail.com>
2021-02-03 14:20:42 -0500

    Update CategoryTile.js

--- client/src/components/CategoryTile.js
+++ client/src/components/CategoryTile.js
@@ -1,14 +1,19 @@
 import React from 'react'
 import { Link } from "react-router-dom"
 
-const CategoryTile = ({category}) => {
+const CategoryTile = ({ category }) => {
   return (
-    <div className="category-tile cell small-6">
-      <Link to={`/categories/${category.id}`}>
-        <h4>
-          {category.name}
-        </h4>
-      </Link>
+    <div className="cell">
+      <div className="card">
+        <img src={ category.imgUrl } />
+        <div className="card-section">
+          <Link to={ `/categories/${category.id}` }>
+            <h4>
+              { category.name }
+            </h4>
+          </Link>
+        </div>
+      </div>
     </div>
   )
 }
